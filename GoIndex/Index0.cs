6c4b7444b00916da9e0116d865779bdf79381e23
Alexander Marchuk <alexander.g.marchuk@gmail.com>
2015-02-17 18:05:52 +0600

    Параллельный проект GoIndex

---
+++ GoIndex/Index0.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PolarDB;
+
+namespace GoIndex
+{
+    class Index0<Tkey> where Tkey: IComparable
+    {
+        internal PaEntry table;
+        internal Func<PaEntry, Tkey> keyProducer;
+        public Index0(string indexName, PaEntry table, Func<PaEntry, Tkey> keyProducer)
+        {
+            this.table = table;
+            this.keyProducer = keyProducer;
+        }
+    }
+}
---
+++ GoIndex/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PolarDB;
+
+namespace GoIndex
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            PaCell table;
+            int maxCount = 100;
+            table = new PaCell(new PTypeSequence(new PTypeRecord(
+                new NamedType("deleted", new PType(PTypeEnumeration.boolean)),
+                new NamedType("name", new PType(PTypeEnumeration.sstring)),
+                new NamedType("age", new PType(PTypeEnumeration.integer)))),
+                "../../table.pac", false);
+            table.Clear();
+            table.Fill(new object[0]);
+            for (int i = 0; i < maxCount; i++)
+            {
+                table.Root.AppendElement(new object[] { false, i.ToString(), i });
+            }
+
+            table.Flush();
+        }
+    }
+}
