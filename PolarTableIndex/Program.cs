10985f845f029d38a1d3cf138076bf72658de576
sergey303 <s@s.s>
2015-02-17 16:02:57 +0600

    start table

--- PolarTableIndex/Program.cs
+++ PolarTableIndex/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PolarDB;
 
 namespace PolarTableIndex
 {
@@ -10,7 +11,17 @@ namespace PolarTableIndex
     {
         static void Main(string[] args)
         {
-
+            var table = new PaCell(new PTypeSequence(new PTypeRecord(
+                new NamedType("deleted", new PType(PTypeEnumeration.boolean)),
+                new NamedType("name", new PType(PTypeEnumeration.sstring)),
+                new NamedType("age", new PType(PTypeEnumeration.integer)))),
+                "../../table.pac", false);
+            table.Clear();
+            table.Fill(new object[0]);
+            for (int i = 0; i < 100; i++)
+            {
+                table.Root.AppendElement(new object[] { false, i.ToString(), i});
+            }
         }
     }
 }
