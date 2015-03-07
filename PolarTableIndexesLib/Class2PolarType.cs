349d75c0c6ec2a94ae527e96f6d78a5b67036f4f
sergey303 <s@s.s>
2015-03-07 12:23:55 +0600

    add lib

---
+++ PolarTableIndexesLib/Class2PolarType.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PolarDB;
+
+namespace PolarTableIndex
+{
+    internal static class Class2PolarType
+    {
+        public static PType ToPolar<T>()
+        {
+            var type = typeof (T);
+            // none = 0,
+            return ToPType(type);
+        }
+
+        public static PType ToPType(Type type)
+        {
+            if (type == typeof (bool))
+                return new PType(PTypeEnumeration.boolean);
+            if (type == typeof (char))
+                return new PType(PTypeEnumeration.character);
+            if (type == typeof (Int32))
+                return new PType(PTypeEnumeration.integer);
+            if (type == typeof (long))
+                return new PType(PTypeEnumeration.longinteger);
+            if (type == typeof (double))
+                return new PType(PTypeEnumeration.real); //  real = 5,
+            if (type == typeof (byte))
+                return new PType(PTypeEnumeration.@byte);
+            if (type == typeof (string)) //fstring = 6, sstring = 7,
+
+                return new PType(PTypeEnumeration.sstring);
+            if (type.IsArray)
+                return new PTypeSequence(ToPType(type.GetElementType())); 
+            //union = 10,
+            //record = 8,
+            return
+                new PTypeRecord(
+                    type.GetProperties().Select(info => new NamedType(info.Name, ToPType(info.PropertyType))).ToArray());
+
+
+
+        }
+
+
+        public static PType ToPolar<TSeq, TElem>() where TSeq : IEnumerable<TElem> //sequence = 9,
+        {
+            return new PTypeSequence(ToPolar<TElem>());
+        }
+    
+    }
+}
---
+++ PolarTableIndexesLib/PaEntryRowsByOffsets.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using PolarDB;
+
+namespace PolarTableIndex
+{
+    internal static class PaEntryRowsByOffsets
+    {
+        public static void ScanRowsByOffsets(this PaEntry root, IEnumerable<long> offsets, Func<long, object, bool> forEachRow)
+        {
+            if (root.Count() == 0) return;
+            var row = root.Element(0);
+            foreach (var offset in offsets)
+            {
+                row.offset = offset;
+                var o = row.Get();
+                if (!forEachRow(offset,o)) return;
+
+            }
+
+        }
+    
+    }
+}
