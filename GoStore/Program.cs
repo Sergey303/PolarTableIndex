980cc55544d0cd34e77961198e4ceaf0a08593ba
Alexander Marchuk <alexander.g.marchuk@gmail.com>
2015-04-12 14:23:36 +0600

    Продолжение работы над GoStore

--- GoStore/Program.cs
+++ GoStore/Program.cs
@@ -13,7 +13,39 @@ namespace GoStore
             Console.WriteLine("Start GoStore.");
             string path = "../../../Databases/";
             var query = Turtle.LoadGraph(@"D:\home\FactographDatabases\dataset\dataset1M.ttl");
-            Console.WriteLine("Load ok. ntriples={0}", query.Count());
+            VeryEasyNametable ven = new VeryEasyNametable();
+            foreach (var triple in query)
+            {
+                ven.InsertOne(triple.subj);
+                ven.InsertOne(triple.pred);
+                if (triple is OTriple) ven.InsertOne(((OTriple)triple).obj);
+            }
+            
+            Console.WriteLine("Load ok. count={0}", ven.Count());
+
+        }
+    }
+    public class VeryEasyNametable
+    {
+        private Dictionary<string, int> dic = new Dictionary<string, int>();
+        private int nextcode = 0;
+        public int InsertOne(string id)
+        {
+            int c;
+            if (dic.TryGetValue(id, out c)) return c;
+            c = nextcode;
+            nextcode++;
+            dic.Add(id, c);
+            return c;
+        }
+        public Func<string, int> GetCodeByString;
+        public VeryEasyNametable()
+        {
+            GetCodeByString = (string id) => dic[id];
+        }
+        public int Count()
+        {
+            return dic.Count;
         }
     }
 }
